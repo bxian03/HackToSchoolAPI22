6dbe071bce09e27091c504a1fc67e15b4ebcb7d0
calv <kelvinwong0519@gmail.com>
2022-09-03 13:06:16 -0700

    confirmed connects to mongodb; relocate to index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+const app = express()
+const port = 3000;
+
+database.on('error', (error) => {
+  console.log(error)
+});
+
+database.once('connected', () => { // will only run once (1x) and once it is connected
+  console.log('Database Connected');
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World, from express');
+});
+
+app.use('/api', routes)
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
