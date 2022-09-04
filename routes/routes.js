2a0e063df96638014347a1fe00fe48588506c408
Brian Xian <brianxian2607@gmail.com>
2022-09-03 17:24:00 -0700

    changed api routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const Model = require("../models/model");
 const router = express.Router();
 
 //Post Method
-router.post("/post", async (req, res) => {
+router.post("/organic_compounds", async (req, res) => {
   try {
     const data = await Model.create(req.body);
     const dataToSave = await data.save();
@@ -15,18 +15,18 @@ router.post("/post", async (req, res) => {
 });
 
 //Get all Method
-router.get("/getAll", async (req, res) => {
+router.get("/organic_compounds", async (req, res) => {
   // works 4:05 pm
   try {
     const data = await Model.find();
-    res.json(data);
+    await Model.res.json(data);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
 //Get by ID Method
-router.get("/getOne/:id", async (req, res) => {
+router.get("/organic_compounds/:id", async (req, res) => {
   // ignore for now
   try {
     const data = await Model.findById(req.params.id);
@@ -37,12 +37,12 @@ router.get("/getOne/:id", async (req, res) => {
 });
 
 //Update by ID Method
-router.patch("/update/:id", (req, res) => {
+router.patch("/organic_compounds/:id", (req, res) => {
   res.send("Update by ID API");
 });
 
 //Delete by ID Method
-router.delete("/delete/:id", (req, res) => {
+router.delete("/organic_compounds/:id", (req, res) => {
   res.send("Delete by ID API");
 });
 
